c9dede6a54763fadd05219ea93d99b4ffd8dc9f8
nicolezca <tatilezcano03@gmail.com>
2023-12-09 18:08:20 -0300

    archivo de componente HEADER

---
+++ proyecto_expo_2023/components/cabecera.php
@@ -0,0 +1,17 @@
+<header class="nab-var">
+        <div class="logo">
+           <h1>industrial</h1>
+        </div>
+        <nav>
+            <ul class="list_link">
+                <li><a href="../home/index.php">Inicio</a></li>
+                <li><a href="#" id="ver">Registro <i class='bx bx-down-arrow-alt arrow'></i></a>
+                    <ul class="list2_link" style="opacity: 0;visibility:hidden;" >
+                        <li><a href="../home/instituciones.php">Instituciones</a></li>
+                        <li><a href="../home/visitas.php">Visitas</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Borrar</a></li>
+            </ul>
+        </nav>
+    </header>
