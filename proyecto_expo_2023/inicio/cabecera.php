7f1af726df364c0eaa982f329ed95e4d3cd5efa6
nicolezca <tatilezcano03@gmail.com>
2023-12-09 17:13:58 -0300

    archivo contenedor de codigo del HEADER

---
+++ proyecto_expo_2023/inicio/cabecera.php
@@ -0,0 +1,17 @@
+<header class="nab-var">
+        <div class="logo">
+           <h1>industrial</h1>
+        </div>
+        <nav>
+            <ul class="list_link">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="#" id="ver">Registro <i class='bx bx-down-arrow-alt arrow'></i></a>
+                    <ul class="list2_link">
+                        <li><a href="instituciones.php">Instituciones</a></li>
+                        <li><a href="visitas.php">Visitas</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Borrar</a></li>
+            </ul>
+        </nav>
+    </header>
