6db0419908e32b7878b9f622329430522596575d
nicolezca <tatilezcano03@gmail.com>
2023-12-10 13:35:16 -0300

    mejora de funcionalidad y limpieza

--- proyecto_expo_2023/database/data/subir_visita.php
+++ proyecto_expo_2023/database/data/subir_visita.php
@@ -1,19 +1,37 @@
 <?php
 include('../bd.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombreCompleto = $_POST["nombre_completo"];
-    $edad = $_POST["edad"];
-    $tipoVisita = $_POST["tipo_visita"];
-    $fechaHoraActual = date('Y-m-d H:i:s');
-    
-    $sql = "INSERT INTO personas (nombre_completo, edad, tipo,tiempo) VALUES ('$nombreCompleto', '$edad', '$tipoVisita','$fechaHoraActual')";
+function limpiarDato($conn, $dato) {
+    return mysqli_real_escape_string($conn, htmlspecialchars($dato));
+}
 
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('visita registrada'); window.location.href = '../../home/visitas.php';</script>";
+function validarDatos($nombre, $edad, $tipoVisita) {
+    // Aquí puedes agregar reglas de validación según tus requisitos
+    return !empty($nombre) && is_numeric($edad) && !empty($tipoVisita);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombreCompleto = limpiarDato($conn, $_POST["nombre_completo"]);
+    $edad = limpiarDato($conn, $_POST["edad"]);
+    $tipoVisita = limpiarDato($conn, $_POST["tipo_visita"]);
+    $fechaHoraActual = date('Y-m-d H:i:s');
+
+    if (validarDatos($nombreCompleto, $edad, $tipoVisita)) {
+        $sql = "INSERT INTO personas (nombre_completo, edad, tipo, tiempo) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("siss", $nombreCompleto, $edad, $tipoVisita, $fechaHoraActual);
+
+        if ($stmt->execute()) {
+            echo "<script>alert('Visita registrada'); window.location.href = '../../home/visitas.php';</script>";
+        } else {
+            echo "Error al guardar los datos: <a href='../../home/visitas.php'>Volver</a>";
+        }
+
+        $stmt->close();
     } else {
-        echo "Error al guardar los datos: <a href='../../home/visitas.php'>Volver</a>";
+        echo "Error: Datos no válidos. <a href='../../home/visitas.php'>Volver</a>";
     }
+
     $conn->close();
 }
 ?>
