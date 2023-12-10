28c7ddb0cf01711a2488c9cea331b64c8f8f74d1
nicolezca <tatilezcano03@gmail.com>
2023-12-10 14:00:04 -0300

    se creo funciones para mejor manejo

--- proyecto_expo_2023/public/js/main.js
+++ proyecto_expo_2023/public/js/main.js
@@ -1,19 +1,25 @@
+const buttonToShowHide = document.getElementById("ver");
+const hiddenListElement = document.querySelector(".list2_link");
+const arrowIcon = document.querySelector(".arrow");
 
-const verElement = document.getElementById("ver");
-const list2LinkElement = document.querySelector(".list2_link");
-const arrow = document.querySelector(".arrow");
-
-
-
-
-verElement.addEventListener("click", function() {
-  if (list2LinkElement.style.opacity === "0") {
-    list2LinkElement.style.opacity = "1";
-    list2LinkElement.style.visibility = "visible";
-    arrow.style.transform ="rotate(180deg)"
+const toggleVisibility = () => {
+  if (hiddenListElement.style.opacity === "0") {
+    showElement();
   } else {
-    list2LinkElement.style.opacity = "0";
-    list2LinkElement.style.visibility = "hidden";
-    arrow.style.transform ="rotate(0)"
+    hideElement();
   }
-});
+};
+
+const showElement = () => {
+  hiddenListElement.style.opacity = "1";
+  hiddenListElement.style.visibility = "visible";
+  arrowIcon.style.transform = "rotate(180deg)";
+};
+
+const hideElement = () => {
+  hiddenListElement.style.opacity = "0";
+  hiddenListElement.style.visibility = "hidden";
+  arrowIcon.style.transform = "rotate(0)";
+};
+
+buttonToShowHide.addEventListener("click", toggleVisibility);
