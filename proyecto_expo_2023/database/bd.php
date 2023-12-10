83b57dab261aaa645db80b0ae0e373d7acbf5bac
nicolezca <tatilezcano03@gmail.com>
2023-12-10 13:33:13 -0300

    se mejoro la seguridad

--- proyecto_expo_2023/database/bd.php
+++ proyecto_expo_2023/database/bd.php
@@ -1,18 +1,19 @@
 <?php
 
-// Conexión a la base de datos (ajusta los valores según tu configuración)
 $servername = 'DB_HOST';
 $username = 'DB_USER';
 $password = 'DB_PASSW0RD';
 $dbname = 'DB_DATABASE';
 
-// Crear la conexión
+// Intentar crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar la conexión
 if ($conn->connect_error) {
-  die('Error de conexión: ' . $conn->connect_error);
+    die('Error de conexión: ' . $conn->connect_error);
 }
 
+// Opcional: Configurar el manejo de errores y excepciones
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 ?>
