b43b4e646f8035ffab4baa149f4696bc6a888c47
nicolezca <tatilezcano03@gmail.com>
2023-12-10 14:00:43 -0300

    se mejoro la seguridad y viabilidad de codigo

--- proyecto_expo_2023/sesion/registro.php
+++ proyecto_expo_2023/sesion/registro.php
@@ -3,36 +3,40 @@
 
 include('../database/bd.php');
 
-// Consulta para obtener el último id registrado en la tabla usuarios
-$sql_last_id = "SELECT MAX(id) AS last_id FROM usuarios";
-$result_last_id = $conn->query($sql_last_id);
-$row_last_id = $result_last_id->fetch_assoc();
-$last_id = $row_last_id['last_id'];
+function obtenerSiguienteID($conn) {
+    $sql = "SELECT MAX(id) AS last_id FROM usuarios";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    return ($row['last_id'] !== null) ? $row['last_id'] + 1 : 1;
+}
 
-// Establecer el valor inicial del id
-if ($last_id === null) {
-    $initial_id = 1;
-} else {
-    $initial_id = $last_id + 1;
+function redirigir($url) {
+    header("Location: $url");
+    exit();
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtener los datos del formulario de registro
-    $nombre_ingreso = $_POST['nombreIngreso'];
-    $usuario_ingreso = $_POST['usuarioIngreso'];
-    $clave_ingreso = $_POST['claveIngreso'];
+    $nombreIngreso = $_POST['nombreIngreso'];
+    $usuarioIngreso = $_POST['usuarioIngreso'];
+    $claveIngreso = $_POST['claveIngreso'];
 
-    // Consulta para insertar el nuevo usuario en la base de datos con el id manual
+    // Obtener el próximo ID disponible
+    $nextID = obtenerSiguienteID($conn);
+
+    // Consulta para insertar el nuevo usuario en la base de datos con el próximo ID
     $sql = "INSERT INTO usuarios (id, nombre, usuario, clave) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("isss", $initial_id, $nombre_ingreso, $usuario_ingreso, $clave_ingreso);
+    $stmt->bind_param("isss", $nextID, $nombreIngreso, $usuarioIngreso, $claveIngreso);
+
     if ($stmt->execute()) {
         // Registro exitoso, redirigir al formulario de inicio de sesión.
-        header("Location:../home/index.php");
-        exit();
+        redirigir('../home/index.php');
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error al registrar el usuario: " . $stmt->error;
     }
+
+    $stmt->close();
 }
 
 $conn->close();
