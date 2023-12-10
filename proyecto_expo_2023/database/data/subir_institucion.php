a1849b19a0c01e7621e13f4089f0d043faec5104
nicolezca <tatilezcano03@gmail.com>
2023-12-10 13:35:07 -0300

    mejora de funcionalidad y limpieza

--- proyecto_expo_2023/database/data/subir_institucion.php
+++ proyecto_expo_2023/database/data/subir_institucion.php
@@ -1,32 +1,50 @@
 <?php
 include('../bd.php');
 
-// Consulta para obtener el último id registrado en la tabla instituciones
-$sql_last_id = "SELECT MAX(id_instituciones) AS last_id FROM instituciones";
-$result_last_id = $conn->query($sql_last_id);
-$row_last_id = $result_last_id->fetch_assoc();
-$last_id = $row_last_id['last_id'];
-
-// Establecer el valor inicial del id
-if ($last_id === null) {
-    $initial_id = 1;
-} else {
-    $initial_id = $last_id + 1;
+function obtenerUltimoID($conn) {
+    $sql = "SELECT MAX(id_instituciones) AS last_id FROM instituciones";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    return ($row['last_id'] !== null) ? $row['last_id'] + 1 : 1;
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombreInstitucion = $_POST["nombre_institucion"];
-    $curso = $_POST["curso"];
-    $cantidadAlumnos = $_POST["cantidad_alumnos"];
-    $cantidadDocentes = $_POST["cantidad_docentes"];
-    $fechaHoraActual = date('Y-m-d H:i:s');
+function limpiarDato($conn, $dato) {
+    return mysqli_real_escape_string($conn, htmlspecialchars($dato));
+}
 
-    $sql = "INSERT INTO instituciones (escuela, curso, cant_alumnos, cant_docentes, id_instituciones, tiempo) VALUES ('$nombreInstitucion', '$curso', '$cantidadAlumnos', '$cantidadDocentes', '$initial_id', '$fechaHoraActual')";
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('instituto registrado'); window.location.href = '../../home/instituciones.php';</script>";
+function validarDatos($nombre, $curso, $cantAlumnos, $cantDocentes) {
+    // Aquí puedes agregar reglas de validación según tus requisitos
+    return !empty($nombre) && !empty($curso) && is_numeric($cantAlumnos) && is_numeric($cantDocentes);
+}
+
+function insertarInstituto($conn, $nombre, $curso, $cantAlumnos, $cantDocentes, $id, $tiempo) {
+    $sql = "INSERT INTO instituciones (escuela, curso, cant_alumnos, cant_docentes, id_instituciones, tiempo) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssiiis", $nombre, $curso, $cantAlumnos, $cantDocentes, $id, $tiempo);
+
+    if ($stmt->execute()) {
+        echo "<script>alert('Instituto registrado'); window.location.href = '../../home/instituciones.php';</script>";
     } else {
         echo "Error al guardar los datos: <a href='../../home/instituciones.php'>Volver</a>";
     }
+
+    $stmt->close();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombreInstitucion = limpiarDato($conn, $_POST["nombre_institucion"]);
+    $curso = limpiarDato($conn, $_POST["curso"]);
+    $cantidadAlumnos = limpiarDato($conn, $_POST["cantidad_alumnos"]);
+    $cantidadDocentes = limpiarDato($conn, $_POST["cantidad_docentes"]);
+    $fechaHoraActual = date('Y-m-d H:i:s');
+
+    if (validarDatos($nombreInstitucion, $curso, $cantidadAlumnos, $cantidadDocentes)) {
+        $lastID = obtenerUltimoID($conn);
+        insertarInstituto($conn, $nombreInstitucion, $curso, $cantidadAlumnos, $cantidadDocentes, $lastID, $fechaHoraActual);
+    } else {
+        echo "Error: Datos no válidos. <a href='../../home/instituciones.php'>Volver</a>";
+    }
+
     $conn->close();
 }
 ?>
