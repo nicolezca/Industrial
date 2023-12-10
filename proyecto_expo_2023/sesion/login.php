040d9b97c2c9ff0e8b2e4d5b3fdcd50cca83b0dc
nicolezca <tatilezcano03@gmail.com>
2023-12-10 14:00:32 -0300

    se mejoro la seguridad y viabilidad de codigo

--- proyecto_expo_2023/sesion/login.php
+++ proyecto_expo_2023/sesion/login.php
@@ -3,6 +3,11 @@
 
 include('../database/bd.php');
 
+function redirigir($url) {
+    header("Location: $url");
+    exit();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtener los datos del formulario
     $loginUsername = $_POST['nombre'];
@@ -17,13 +22,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows == 1) {
         // Inicio de sesión exitoso, redirigir al index o a la página deseada.
-        header("Location: ../home/index.php");
-        exit();
+        redirigir('../home/index.php');
     } else {
         // Inicio de sesión fallido
-        echo "Inicio de sesión fallido. Por favor, verifica tus datos.";
-        echo '<a href="login.html">volver a inciar</a>';
+        echo "Inicio de sesión fallido. Por favor, verifica tus datos. <a href='login.html'>Volver a iniciar</a>";
     }
+
+    $stmt->close();
 }
 
 $conn->close();
